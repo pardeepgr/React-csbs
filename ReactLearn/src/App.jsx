f017a9bc1bd529d486c687bbeb297d6e0f10ba5a
pardeepgr <pks189137@gmail.com>
2026-06-10 11:04:05 +0530

    todo app for learning redux toolkit

---
+++ ReactLearn/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import AddTodo from './components/addTodo'
+import Todos from './components/Todos'
+
+function App() {
+  return (
+    <div >
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </div>
+  )
+}
+
+export default App
+
